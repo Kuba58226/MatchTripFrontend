ae36e3d2ada92d41188827575bc52c6c0c6e602d
Kuba <kubaozut@gmail.com>
2026-03-16 00:27:52 +0100

    Installed packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,15 @@ export default defineNuxtConfig({
   typescript: {
     strict: true
   },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/_colors.scss" as *;',
+        },
+      },
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
